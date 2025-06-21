742fcd3a5d7f86bdecf6eb3ea75bc8d725ece4ca
GeneralBorN <bornabobohorina@gmail.com>
2025-06-21 16:46:54 +0200

    Finishing touches
    Added images for articles, added correct contact email.

--- kontakt.php
+++ kontakt.php
@@ -4,6 +4,6 @@ include 'includes/header.php';
 ?>
 <div class="container">
   <h2>Kontakt</h2>
-  <p>Za sva pitanja pišite na <a href="mailto:info@example.com">info@example.com</a>.</p>
+  <p>Za sva pitanja pišite na <a href="mailto:bhorina@tvz.hr">bhorina@tvz.hr</a>.</p>
 </div>
 <?php include 'includes/footer.php'; ?>
